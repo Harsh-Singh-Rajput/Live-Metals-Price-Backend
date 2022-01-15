0d2e25c730e92488f313ac7f9bf14f43a6b64583
Harsh-Singh-Rajput <61553586+Harsh-Singh-Rajput@users.noreply.github.com>
2022-01-15 15:14:34 +0530

    Update price.js

--- price.js
+++ price.js
@@ -8,13 +8,8 @@ const env = require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-const corsOptions ={
-   origin:'https://metalprice.netlify.app', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.post("/getPrice", async function (req, res) {
     console.log("In server 2");
